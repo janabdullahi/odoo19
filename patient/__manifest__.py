775afb8200f690c08744fd94bc94d3ea56078a41
abdullahi <jan.ab1517@gmail.com>
2026-05-02 15:39:18 +0100

    removed unncessary files and modified patient model,view and manifest

--- patient/__manifest__.py
+++ patient/__manifest__.py
@@ -26,11 +26,6 @@ Long description of module's purpose
     'data': [
         'security/ir.model.access.csv',
         'views/views.xml',
-        'views/templates.xml',
-    ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
     ],
 }
 
