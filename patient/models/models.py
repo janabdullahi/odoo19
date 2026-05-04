bea92e820a6b85b3f9323268a6cbf4ce694fe44b
abdullahi <jan.ab1517@gmail.com>
2026-05-04 12:36:32 +0100

    add nationaly, martial status and gender fields

--- patient/models/models.py
+++ patient/models/models.py
@@ -11,3 +11,19 @@ class patient(models.Model):
     middle_name = fields.Char()
     last_name = fields.Char()
     dob = fields.Date(string="Date of birth", default=fields.Date.context_today, required=True)
+    gender = fields.Selection([
+        ('male', 'Male'),
+        ('female', 'Female'),
+        ('other', 'Other'),
+        ('unspecified', 'Unspecified'),
+        ], string="Gender", default='unspecified')
+    nationality_id = fields.Many2one('res.country', string='Nationality')
+    martial_status = fields.Selection([
+        ('single', 'Single'),
+        ('married', 'Married'),
+        ('civil partnered', 'Civil Partnered'),
+        ('widowed', 'Widowed'),
+        ('divorced', 'Divorced'),
+        ('separated', 'Separated')
+    ])
+    
